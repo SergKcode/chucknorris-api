22894f0df590f7b942999456bab4e44dc36a4f06
unknown <sergio-caballero_student2021@wilder.school>
2021-04-15 10:44:02 +0200

    api fetch fixed

--- src/components/ShowMemes.js
+++ src/components/ShowMemes.js
@@ -1,22 +1,29 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 
 export const ShowMemes = () => {
     let [api, setApi] = useState('')
     let [apiloaded, setApiloaded] = useState(false)
 
-    fetch('https://api.chucknorris.io/jokes/random')
-    .then(response => response.json())
-    .then(data => {
-        setApi(data)
-        setApiloaded(true)
+    const getDatos=()=>{
+        fetch('https://api.chucknorris.io/jokes/random')
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            setApi(data)
+            setApiloaded(true)
+        }
+        ) 
     }
-    ) 
+    
+    useEffect(() => {
+        getDatos()
 
+    }, [])
 
 
     return (
         <div> 
-     {
+            {/* {
             apiloaded &&
             <div>
               {
@@ -24,7 +31,7 @@ export const ShowMemes = () => {
                 )
               }
               </div>
-          }
+          } */}
           </div>
     )
 }
